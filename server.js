22c78e59bb8e1e580ee073e68e29718e625ebe0c
Joao Pedro <joaopedro.santosoliveira2003@gmail.com>
2022-09-16 22:14:56 -0300

    teste commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt-node');
+const cors = require("cors");
+const knex = require('knex');
+const { json } = require('express');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile')
+const image = require('./controllers/image')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        port: '5432',
+        password: 'w2m302302',
+        database: 'smart-brain'
+    }
+})
+app.use(express.json())
+app.use(cors())
+
+app.post('/signin',signin.handleSignin(db, bcrypt))
+
+app.post('/register',(req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+
+app.put('/image',(req, res) => {image.handleImage(req, res, db)})
+
+app.post('/imageurl',(req, res) => {image.handleApiCall(req, res)})
+
+app.listen(3000, () => {
+    console.log("funcionou")
+})
+

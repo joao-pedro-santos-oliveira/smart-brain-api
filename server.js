2816da85787bebd3b417f39c1e7303ed570f227c
Joao Pedro <joaopedro.santosoliveira2003@gmail.com>
2022-09-16 23:33:05 -0300

    adicionando novo host database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-rigid-80370',
         user: 'postgres',
         port: '5432',
         password: 'w2m302302',
